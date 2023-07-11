a41c201bd51c0f93b4be4b557fe891067723e701
sebaaguayo <sebaaguayoc@gmail.com>
2023-07-11 21:48:17 +0000

    landingupdate

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -1,32 +1,21 @@
 import React from "react";
 
-function Jumbotron(props) {
+function Footer() {
   return (
-    <div className="col-md-12 g-0">
-      <div className="rounded-3 position-relative w-100">
-        <div className="position-absolute top-50 start-50 translate-middle mb-5 w-100 p-5">
-          <h1 className="display-5 fw-bold opacity-80 bg-light text-primary">
-            {props.titulo1}{" "}
-          </h1>
-          <p className="col-md-8 fs-4 opacity-80 bg-light text-primary">
-            {props.parrafo1}
-          </p>
-          <button className="btn btn-success btn-lg mx-3" type="button">
-            Log In
-          </button>
-          <button className="btn btn-success btn-lg" type="button">
-            Sign Up
-          </button>
+    <div class="container-fluid bg-dark">
+      <footer class="d-flex justify-content-center py-3">
+        <div class="col-md-4 d-flex align-items-center">
+          <a
+            href="/"
+            class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
+          >
+            <svg class="bi" width="30" height="24"></svg>
+          </a>
+          <span class="mb-3 mb-md-0 text-muted">© 2022 Company, Inc</span>
         </div>
-        <img
-          src={props.imagen1}
-          className="card-img-top w-100"
-          style={{ objectFit: "cover", height: "400px" }}
-          alt="..."
-        />
-      </div>
+      </footer>
     </div>
   );
 }
 
-export default Jumbotron;
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,87 @@
-import React from "react";
+//import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+//const Home = () => {
+//return (
+//<div className="text-center">
+//<h1 className="text-center mt-5">Hello Rigo!</h1>
+//<p>
+//	<img src={rigoImage} />
+//</p>
+//		<a href="#" className="btn btn-success">
+//			If you see this green button... bootstrap is working...
+//		</a>
+//		<p>
+//			Made by{" "}
+//			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+//			love!
+//		</p>
+//	</div>
+//	);
+//};
 
-export default Home;
+import React from "react";
+import Card from "./card.jsx";
+import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbojumbotron.jsx";
+import Footer from "./footer.jsx";
+//todo es componente
+//un componente es una funcion XD pero  que inicia con Mayuscula
+//los componentes se llaman como si fueran una etiqueta HTML
+//las fnciones normales las llamamos, return any, y se llaman X()
+//si creamos un componente tiene que tener MAYUSCULA!!!
+//import nombre funcion from "./component/home.jsx";
+function Cimiento() {
+  return (
+    <>
+      <Navbar />
+      <div className="container-fluid">
+        <div className="row">
+          <Jumbotron
+            titulo1="Welcome to InnovationAvantGarde"
+            parrafo1="Wondering about the future of human kind? Here you will find the best topics on science, music, drugs and more. Remember to sign up to our newsletter regarding more information on the best online journal through the world of avantgarde"
+            imagen1="https://img.freepik.com/vector-gratis/fondo-abstracto-lineas-blancas-horizontales-dinamicas_1017-33891.jpg?size=626&ext=jpg&uid=R95930245&ga=GA1.1.444628794.1678494005&semt=sph"
+          />
+          <div className="col-md-3">
+            <Card
+              imagen="https://img.freepik.com/vector-gratis/bandera-dia-internacional-contra-uso-indebido-trafico-ilicito-drogas_23-2149414239.jpg?size=626&ext=jpg&ga=GA1.2.444628794.1678494005&semt=popular"
+              titulo="Drug Abuse"
+              parrafo="Fighting addiction is difficult task for anyone"
+            />
+          </div>
+          <div className="col-md-3">
+            <Card
+              imagen="https://img.freepik.com/foto-gratis/piedras-zen-apiladas-fondo-arena-arte-concepto-equilibrio_53876-110629.jpg?size=626&ext=jpg&uid=R95930245&ga=GA1.2.444628794.1678494005&semt=sph"
+              titulo="The use of Meditation for drug abuse"
+              parrafo="When people are frustrated trying"
+            />
+          </div>
+          <div className="col-md-3">
+            <Card
+              imagen="https://img.freepik.com/vector-gratis/fondo-azul-morado-dia-mundial-contra-cancer_23-2147734364.jpg?size=626&ext=jpg&uid=R95930245&ga=GA1.1.444628794.1678494005&semt=sph"
+              titulo="Why do people get Cancer?"
+              parrafo="There is a lot of debate about the origin of all the types of Cancer diseases, but when it comes to non-hereditary cancer, we ask ourselves, how is that possible? there's a lot of people who gets cancer by psychomatic consequences."
+            />
+          </div>
+          <div className="col-md-3">
+            <Card
+              imagen="https://img.freepik.com/vector-gratis/vector-elemento-diseno-ilusion-agujero-gusano-cuadricula-3d_53876-140887.jpg?size=626&ext=jpg&uid=R95930245&ga=GA1.1.444628794.1678494005&semt=ais"
+              titulo="Satalite James Webb new discovery"
+              parrafo="NASA has publicated a new article about an amazing new research on a event horizon using their greatest Space Telescope"
+            />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Cimiento;
+
+//se diferencian los  componentes  y las funciones, es que la primera son funciones que retornan HTML
+//funciones tienen que ser exportadas para poder utilizadas por otros archivos
+//antes module.export, ahora export default "nombre componente"
